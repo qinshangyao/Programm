c4716d09f72253b61868bba4601c3e6580ab9b90
qinshangyao <shangyao_qin@163.com>
2018-08-12 00:12:50 +0800

    the last version

--- tmp.sh
+++ tmp.sh
@@ -1,6 +1,6 @@
 echo "Name,Min,Max,Mean,StdDev,Median,Sum,Cout" > filterdata.csv
 ls ./HelpCaiyu | while read id
 do 
-	awk -v name=$id 'BEGIN{FS=","} NR==9{print name"," $2","$3","$4","$5","$6","$7","$8}' ./HelpCaiyu/$id
+	awk -v name=$id 'BEGIN{FS=","} /Cell Number of Voxels/{print name"," $2","$3","$4","$5","$6","$7","$8}' ./HelpCaiyu/$id
 done >> filterdata.csv
  
