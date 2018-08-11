648a7de6b16666cc66c885afa7a099c2f348a0c6
qinshangyao <shangyao_qin@163.com>
2018-08-11 23:46:17 +0800

    done--2018-08-11:23,46

--- tmp.sh
+++ tmp.sh
@@ -1,4 +1,6 @@
-ls ./Imaris | while read id
-do
-	awk 'BEGIN{FS=","}{printif %10s,%10s,%10s,%10s,%10s,%10s,%10s,%10s\n,"Name","Min","Max","Mean","StdDev","Median","Sum","Cout"} NR==9{print $1 $2 $3 $4 $5 $6 $7 %8}' ./Imaris/$id 
-done > filterdata.csv
+echo "Name,Min,Max,Mean,StdDev,Median,Sum,Cout" > filterdata.csv
+ls ./HelpCaiyu | while read id
+do 
+	awk -v name=$id 'BEGIN{FS=","} NR==9{print name"," $2","$3","$4","$5","$6","$7","$8}' ./HelpCaiyu/$id
+done >> filterdata.csv
+ 
