f04e8849c255300406075abd014ce22f31ee9298
qinshangyao <shangyao_qin@163.com>
2018-08-12 09:13:42 +0800

    modify tmp2.sh in master

--- tmp2.sh
+++ tmp2.sh
@@ -1,8 +1,9 @@
 # tmp2.sh
+this line was at master branch;you decide 
 
 printf "%5s %5s %5s %5s %5s %5s %5s %5s\n" "Name","Min","Max","Mean","StdDev","Median","Sum","Cout"  > filterdata.csv
 ls ./HelpCaiyu | while read id
 do 
 	awk -v name=$id 'BEGIN{FS=","} /Cell Number of Voxels/{print name"," $2","$3","$4","$5","$6","$7","$8}' ./HelpCaiyu/$id
-done >> filterdata.csv
+done >> filterdata2.csv
 
